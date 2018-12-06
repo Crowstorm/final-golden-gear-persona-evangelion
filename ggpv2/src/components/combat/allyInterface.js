2e75d02596e67688bd467b69040d3ac5b562170c
Crowstorm <crowstormofficial@gmail.com>
2018-12-06 08:44:25 +0100

    bar progress

--- ggpv2/src/components/combat/allyInterface.js
+++ ggpv2/src/components/combat/allyInterface.js
@@ -12,14 +12,25 @@ class AllyInterface extends React.Component {
             return (
                 <div key={char.name} className="d-flex flex-row">
                     {/* mana bar */}
-                    <Bar percentage={70} />
+                    <div>
+                    <Bar
+                        percentage={90}
+                        type="health"
+                        side="ally"
+                    />
+                    </div>
+                
                     <div>
                         <div className="characterNameContainer d-flex justify-content-center">
                             {char.name}
                         </div>
                         <img className="characterPortrait" alt="ally" src={char.portrait} />
                     </div>
-                    <Bar percentage={30} />
+                    <Bar
+                        percentage={30}
+                        type="mana"
+                        side="ally"
+                    />
 
 
                     {/* hp bar */}
--- ggpv2/src/components/combat/bar.js
+++ ggpv2/src/components/combat/bar.js
@@ -2,19 +2,37 @@ import './combat.css';
 
 import React from 'react';
 
-export const Bar = (props) =>{
-    return(
+export const Bar = (props) => {
+    return (
         <div className="bar">
-            <Filler percentage={props.percentage}/>
+            {/* <Filler percentage={props.percentage} side={props.side} type={props.type} /> */}
+            <Value percentage={props.percentage} />
         </div>
     )
 }
 
-export const Filler = (props) =>{
+export const Filler = (props) => {
     console.log(props)
-    return(
-        <div className="barFiller" style={{height: `${props.percentage}%`}}>
+    if (props.side === 'ally') {
+        return (
+            <div className="barFiller" style={{
+                height: `${props.percentage}%`, background: "#a51d1d"
+            }}>
+                {/* {props.percentage} */}
+            </div>
+        )
+    }
+    return (
+        <div className="barFiller" style={{ height: `${props.percentage}%` }}>
 
+        </div>
+    )
+}
+
+const Value = (props) =>{
+    return(
+        <div className="barText " style={{transform: "rotate(-90deg)", color: "white", }}>
+            {props.percentage}
         </div>
     )
 }
